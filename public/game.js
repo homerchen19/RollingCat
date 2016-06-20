da1460d52eec3f7d74bcf038633e9c25f2fd0e87
xxhomey19 <xxhomey19@gmail.com>
2016-06-20 16:37:01 +0800

    change finalnum to 20

--- public/game.js
+++ public/game.js
@@ -28,7 +28,7 @@
 	var game = new Phaser.Game(innerWidth, innerHeight, Phaser.AUTO, '');
 	var randomXPosition = game.rnd.integerInRange(innerWidth, innerWidth * 1.5);
 	var current_page = 1;
-	var finalnum = 50;
+	var finalnum = 20;
   var die_num = 0;
 	game.state.add('boot', bootState);
 	game.state.add('load_node', loadState);
